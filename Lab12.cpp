220fd0c511f26bc38304323e638b366064e95e88
AlexBarbachov <66849795+AlexBarbachov@users.noreply.github.com>
2025-09-21 20:16:53 -0700

    third

--- Lab12.cpp
+++ Lab12.cpp
@@ -9,11 +9,33 @@ void readGrades(array<int, 30>& classGrades, const string& filename);
 
 // program will be about an array of class grades.
 int main()
-{
+{  
+    array <int, 30> classGrades;
+
+
+    readGrades(classGrades, "Lab12.txt");
+
+    // quickly output the grades
+    cout << "Grades" << endl;
+    for (int i = 0; i < classGrades.size() < i++)
+    {
+        cout << "Student " << i + 1 << " | " << classGrades.at(i);
+    }
+
+    // DIFFERENT ARRAY METHODS
+
+
+    // will give first and last element respectively 
+    cout << "First: " << classGrades.front() << endl;
+    cout << "Last: " << classGrades.back() << endl;
+
+    cout << "Number of Grades: " << classGrades.size() << endl; 
+
 
 }
 
-//
+// will read the file and store it into an array: classGrades
+// will manipulate array in main fdunction
 void readGrades(array<int, 30>& classGrades, const string& filename)
 {
     ifstream file(filename);

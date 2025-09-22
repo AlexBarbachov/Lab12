0289868fb0777f0c0289ceca2530b52abaa4de4d
AlexBarbachov <66849795+AlexBarbachov@users.noreply.github.com>
2025-09-21 20:11:57 -0700

    second comm

--- Lab12.cpp
+++ Lab12.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
+#include <array>
+#include <fstream>
 
+using namespace std;
+
+void readGrades(array<int, 30>& classGrades, const string& filename);
 
 
 // program will be about an array of class grades.
@@ -7,3 +12,26 @@ int main()
 {
 
 }
+
+//
+void readGrades(array<int, 30>& classGrades, const string& filename)
+{
+    ifstream file(filename);
+    if (!file)
+    {
+        cout << "Error opening file" << endl;
+        return;
+    }
+
+    int val;
+    for (size_t t; t < classGrades.size(); t++)
+    {
+        if (file >> val)
+        {
+            classGrades.at(t) = val;
+        }
+    }
+
+    file.close();
+}
+
